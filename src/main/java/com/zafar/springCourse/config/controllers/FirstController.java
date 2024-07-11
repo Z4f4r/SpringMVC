d0e9314bd1cdbf75cb4d4cd6cdda3c2edfa77c8d
Zafarjon <idk677799@gmail.com>
2024-07-11 07:27:49 +0300

    6

--- src/main/java/com/zafar/springCourse/config/controllers/FirstController.java
+++ src/main/java/com/zafar/springCourse/config/controllers/FirstController.java
@@ -4,12 +4,29 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.RequestParam;
+
 @Controller
 // @RequestMapping("/first") -> /first/hello, /first/goodbye
 public class FirstController {
 
-    @GetMapping("/hello")
-    public String helloPage() {
+//    @GetMapping("/hello")
+//    public String helloPage(HttpServletRequest request) {
+//        String name = request.getParameter("name");
+//        String surname = request.getParameter("surname");
+//
+//        System.out.println("Hello, " + name + " " + surname);
+//
+//        return "first/hello";
+//    }
+
+    @GetMapping("/hello") // required = true means we shall get request only with parameters
+    public String helloPage(@RequestParam(value = "name", required = false) String name,
+                            @RequestParam(value = "surname", required = false) String surname) {
+
+        System.out.println("Hello, " + name + " " + surname);
+
         return "first/hello";
     }
 
